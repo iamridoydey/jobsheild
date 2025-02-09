722217cfb5064646d7aec18638edab453977f9e9
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-02-10 00:19:00 +0600

    Upload image to the cloudinary

---
+++ src/lib/imageOperator.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const handleUploadImages = async (images: File[]) => {
+  const urls = await Promise.all(images.map((image) => uploadFile(image)));
+  return urls.filter((url) => url !== null);
+};
+
+export const handleUploadLogo = async (image: File) => {
+  if (!image) return null;
+  return await uploadFile(image);
+};
+
+const uploadFile = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "jobsheild_uploads");
+
+    try {
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    console.log("response : ", response);
+
+    if (!response.ok) {
+      throw new Error("Failed to upload image");
+    }
+
+    const data = await response.json();
+    console.log("Cloudinary Response: ", data);
+    return data.secure_url;
+  } catch (error) {
+    console.error("Upload Error:", error);
+    return null;
+  }
+};
