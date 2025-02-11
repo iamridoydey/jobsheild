1c2d0426190a840733407702c12e6c9ead2ecace
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-02-11 13:04:12 +0600

    Updated handler function for graphql route

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -3,18 +3,14 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextRequest } from "next/server";
 import typeDefs from "@/app/graphql/schema.graphql";
 import resolvers from "@/app/graphql/resolvers";
-import { NextApiRequest, NextApiResponse } from "next";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Define the handler function that properly handles the context
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const nextHandler = startServerAndCreateNextHandler<NextRequest>(server);
-  return nextHandler(req, res);
-};
+// Define the handler function to handle the request and context properly
+const handler = startServerAndCreateNextHandler<NextRequest>(server);
 
 export const GET = handler;
 export const POST = handler;
