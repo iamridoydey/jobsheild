aae76377f43cbe0a6be7e49231a28b18dc8496cf
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-02-11 09:38:03 +0600

    Added ImportantLink, Frauder interface

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -8,3 +8,31 @@ export interface HR {
   account: string;
   accountUrl: string;
 }
+
+export interface ImportantLink {
+  key: string;
+  value: string;
+}
+
+export interface Frauder {
+  _id: string;
+  companyName: string;
+  logo: string;
+  hrList: HR[];
+  importantLinks: ImportantLink[];
+  createdBy: string;
+  contributors: string[];
+  isJustified: boolean;
+  proofs: string[];
+  createdAt: string;
+}
+
+export interface ProofData {
+  _id: string;
+  frauderId: string;
+  submittedBy: string;
+  screenshots: string[];
+  description: string;
+  isJustified: boolean;
+  createdAt: string;
+}
