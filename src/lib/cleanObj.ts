47a433f8af2a947a779482c6cea0c6bb8820430d
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-02-11 12:38:39 +0600

    Disabled no-explicit-any

--- src/lib/cleanObj.ts
+++ src/lib/cleanObj.ts
@@ -2,6 +2,7 @@
 export function cleanObject(obj: any) {
   return Object.fromEntries(
     Object.entries(obj).filter(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       ([_, v]) => v !== "" && v !== null && v !== undefined
     )
   );
