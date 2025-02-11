5201aea9976fc56506757022732da8bb0d523992
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-02-11 20:06:51 +0600

    Updated server to solve cors problem

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,10 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextRequest, NextResponse } from "next/server";
 import typeDefs from "@/app/graphql/schema.graphql";
 import resolvers from "@/app/graphql/resolvers";
-
-
 
 // Initialize Apollo Server
 const server = new ApolloServer({
@@ -19,12 +18,27 @@ const handler = startServerAndCreateNextHandler<NextRequest>(server, {
 
 // Wrap the handler to add CORS headers
 const wrappedHandler = async (req: NextRequest) => {
-  const response = await handler(req);
-  const headers = new Headers(response.headers);
-  headers.set("Access-Control-Allow-Origin", "*");
-  headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  headers.set("Access-Control-Allow-Headers", "Content-Type");
-  return new NextResponse(response.body, { headers });
+  try {
+    const response = await handler(req);
+    const headers = new Headers(response.headers);
+    headers.set("Access-Control-Allow-Origin", "*");
+    headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    headers.set("Access-Control-Allow-Headers", "Content-Type");
+    return new NextResponse(response.body, { headers });
+  } catch (error: any) {
+    console.log("Error : ", error.message);
+    const headers = new Headers();
+    headers.set("Access-Control-Allow-Origin", "*");
+    headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    headers.set("Access-Control-Allow-Headers", "Content-Type");
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      {
+        status: 500,
+        headers,
+      }
+    );
+  }
 };
 
 // Handle OPTIONS method for CORS preflight
